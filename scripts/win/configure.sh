d040d234b969e85a1d5da0bfaf15a818dfcb0367
Bohdan Semenov <bodyasemzen@gmail.com>
2026-08-18 22:41:28 +0300

    add configure.sh

---
+++ scripts/win/configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+script_dir=$(dirname $0)
+root_dir=$(realpath "$script_dir/../..")
+
+cmake -S "$root_dir" -B ./build \
+    -G "Visual Studio 18 2026" \
+    -A x64 \
+    -T "version=14.50" \
+    -DBUILD_EXAMPLES=ON \
+    -DCMAKE_TOOLCHAIN_FILE=C:/my_space/code/vcpkg/scripts/buildsystems/vcpkg.cmake \
+    -DUSE_OIIO=ON \
+    -DHIP_ARCHITECTURES=gfx1201
